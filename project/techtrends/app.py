282f76e1498d0fdfb4f88240bf0bf92f0e05a68f
Venkat Repaka <venkat.repaka@gmail.com>
2023-09-12 13:28:31 +0530

    logs added

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,20 +2,42 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+from logging.config import dictConfig
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                "datefmt": "%B %d, %Y %H:%M:%S %Z",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "DEBUG", "handlers": ["console"]},
+    }
+)
 
 connections_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connections_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connections_count += 1
     return connection
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    connections_count += 1
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
@@ -29,7 +51,6 @@ app.config['SECRET_KEY'] = 'your secret key'
 @app.route('/')
 def index():
     connection = get_db_connection()
-    connections_count += 1
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
@@ -40,13 +61,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.debug('A non-existing article is accessed and a 404 page is returned')
       return render_template('404.html'), 404
     else:
+      app.logger.debug('Article titled -> {title} is retreived'.format(title=post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.debug('The "About Us" page is retrieved')
     return render_template('about.html')
 
 @app.route('/healthz')
@@ -61,7 +85,7 @@ def metrics():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
-    post_count = size(posts)
+    post_count = len(posts)
     return jsonify({"post_count": post_count, "db_connection_count": connections_count})
 
 # Define the post creation functionality 
@@ -75,16 +99,16 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connections_count += 1
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.debug('Article titled -> {title} is created'.format(title=title))
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
+   app.logger.debug('Flask server starting')
    app.run(host='0.0.0.0', port='3111')
